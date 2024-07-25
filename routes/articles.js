e5987375671786e4a78ed6511dee8069685accfa
m-lois-bl <marion.blayo@yahoo.fr>
2024-07-25 13:38:42 +0200

    Début externalisation des routes

--- routes/articles.js
+++ routes/articles.js
@@ -1,55 +1,9 @@
-// ==================== Imports : Début ====================
-const express = require('express');
-const { body, validationResult } = require('express-validator');
-const { v4: uuid } = require('uuid');
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-// ==================== Imports : Fin ====================
-
-
-//Instanciation de l'application serveur
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-// Simulation de données en mémoire
-let articles = [
-    { id: 1, title: 'Premier article', content: 'Contenu du premier article', author: 'Isaac' },
-    { id: 2, title: 'Deuxième article', content: 'Contenu du deuxième article', author: 'Sanchez' },
-    { id: 3, title: 'Troisième article', content: 'Contenu du troisième article', author: 'Toto' }
-];
-
-// ==================== Configuration de la BDD : Début  ====================
-mongoose.connection.once('open', () => {
-    console.log("Connecté à la BDD.");
-});
-mongoose.connection.on('error', () => {
-    console.log(`Erreur de bdd : ${err}.`);
-})
-// Articles
-mongoose.connect('mongodb://localhost:27017/db_articles');
-const Article = mongoose.model('Article', {
-    uuid : String,
-    title : String,
-    content : String,
-    author: String
-}, 'articles');
-
-// Utilisateurs
-const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-  });
-  
-userSchema.plugin(uniqueValidator);
-  
-module.exports = mongoose.model('User', userSchema);
-
-// ==================== Configuration de la BDD : Fin  ====================
+const express= require('express');
+const router = express.Router();
 
 
 // ==================== Déclaration des routes ====================
-app.get('/articles', async (request, response) => {
+router.get('/articles', async (request, response) => {
     const articles = await Article.find();
     return response.status(200).json({
         code: "200",
@@ -57,7 +11,7 @@ app.get('/articles', async (request, response) => {
         data: articles});
 })
 
-app.get('/article/:id', async (request, response) => {
+router.get('/article/:id', async (request, response) => {
     const uuidParam = request.params.id;
 
     const existingArticle = await Article.findOne({ uuid: uuidParam });
@@ -71,7 +25,7 @@ app.get('/article/:id', async (request, response) => {
     });
 })
 
-app.post('/save-article',
+router.post('/save-article',
     async (request, response) => {
 
     const articleJSON = request.body;
@@ -111,7 +65,7 @@ app.post('/save-article',
   
 })
 
-app.delete('/article/:id', async (request, response) => {
+router.delete('/article/:id', async (request, response) => {
     const uuidParam = request.params.id;
     const existingArticle = await Article.findOne({ uuid: uuidParam });
     if(existingArticle){
@@ -122,10 +76,6 @@ app.delete('/article/:id', async (request, response) => {
     
 })
 
-app.listen(3000, () =>{
-    console.log('L\'application à bien démarré');
-})
-
 function createJsonResponse(code, message, data){
     return {
         code: code,
@@ -133,3 +83,6 @@ function createJsonResponse(code, message, data){
         date: data
     };
 }
+
+
+module.exports = router;
